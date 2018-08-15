6979fc1e633ebff4992d336fff95227ee125bd93
崔睿峰 <32936078+ailec0623@users.noreply.github.com>
2018-08-15 09:43:53 +0800

    Update 2.py
    1

--- spider/2.py
+++ spider/2.py
@@ -6,7 +6,7 @@ url = "https://coes-stud.must.edu.mo/coes/login.do"
 info = "https://coes-stud.must.edu.mo/coes/StudentInfo.do"
 logout = "https://coes-stud.must.edu.mo/coes/logout.do"
 postdata = urllib.parse.urlencode({"userid":"1509853V-I011-0026",
-                                   "password":"25391839"}).encode('utf-8')
+                                   "password":"++++++++"}).encode('utf-8')
 head1=('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0')
 
 cj = http.cookiejar.CookieJar()
